2bdd8b6a738155367561b88e7f51d3d43590839d
swetha140803 <busyswetha2003@gmail.com>
2026-04-08 17:30:22 +0530

    Update

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const rootStyles = getComputedStyle(document.documentElement);
+
+const NAV_BG = rootStyles.getPropertyValue('--bg-deep-transparent');
+const NAV_SOLID = rootStyles.getPropertyValue('--bg-deep');
+
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".custom-nav");
 
   if (window.scrollY > 50) {
-    nav.style.background = "#071520";
+    nav.style.background = NAV_SOLID;
+  } else {
+    nav.style.background = NAV_BG;
   }
 });
