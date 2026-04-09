ff6be154ba7ab266de8bf8daed7110c45458cb88
swetha140803 <busyswetha2003@gmail.com>
2026-04-09 18:04:30 +0530

    Updated UI

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const rootStyles = getComputedStyle(document.documentElement);
 
-const NAV_BG = rootStyles.getPropertyValue('--bg-deep-transparent');
-const NAV_SOLID = rootStyles.getPropertyValue('--bg-deep');
+const NAV_TRANSPARENT = rootStyles.getPropertyValue('--nav-transparent');
+const NAV_SOLID = rootStyles.getPropertyValue('--bg-primary');
+
+const nav = document.querySelector(".custom-nav");
 
 window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".custom-nav");
-
   if (window.scrollY > 50) {
     nav.style.background = NAV_SOLID;
   } else {
-    nav.style.background = NAV_BG;
+    nav.style.background = NAV_TRANSPARENT;
   }
 });
